8dde044761bc9aeb7db4911511cb5a09862d85b7
God or AFK <55185077+GodOrAFK@users.noreply.github.com>
2021-01-05 19:16:42 +0100

    Added SettingMenu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public GameObject SoundController;
+
+    [Header("Sub-Menus")]
+    public GameObject MainMenu;
+    public GameObject SettingMenu;
+
+    private void Awake()
+    {
+        if (SoundController != null) { DontDestroyOnLoad(SoundController); }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +42,20 @@ public class MenuController : MonoBehaviour
         else
         {
             Application.Quit();
+        }
+    }
+
+    public void ToggleSettings(bool open)
+    {
+        if (open)
+        {
+            MainMenu.SetActive(false);
+            SettingMenu.SetActive(true);
+        }
+        else
+        {
+            MainMenu.SetActive(true);
+            SettingMenu.SetActive(false);
         }
     }
 }
