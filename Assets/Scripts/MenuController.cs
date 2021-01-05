82058fd8860ee67ef04694a41a650c61b3186c76
God or AFK <55185077+GodOrAFK@users.noreply.github.com>
2021-01-05 16:34:20 +0100

    Created Main Menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("World");
+    }
+
+    public void Exit()
+    {
+        if (Debug.isDebugBuild)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float _length;
+    private float _startPos;
+
+    public GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = transform.position.x;
+        _length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(_startPos + dist, transform.position.y, transform.position.z);
+
+        if(temp > _startPos + _length) { _startPos += _length; }
+        else if(temp < _startPos - _length) { _startPos -= _length; }
+    }
+}
